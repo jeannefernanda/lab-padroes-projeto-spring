0501503fc3c7b930de502f9cd89c9d78922acb68
Jeanne <jeannefernanda.m@gmail.com>
2024-06-29 11:42:31 -0400

    Initial commit

---
+++ src/main/java/one/digitalinnovation/gof/LabPadroesProjetoSpringApplication.java
@@ -0,0 +1,24 @@
+package one.digitalinnovation.gof;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+/**
+ * Projeto Spring Boot gerado via Spring Initialzr
+ * Os seguintes módulos foram selecionados:
+ * - Spring Data JPA
+ * - Spring Web
+ * - H2 Database
+ * - OpenFeign
+ */
+
+@EnableFeignClients
+@SpringBootApplication
+public class LabPadroesProjetoSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LabPadroesProjetoSpringApplication.class, args);
+	}
+
+}
